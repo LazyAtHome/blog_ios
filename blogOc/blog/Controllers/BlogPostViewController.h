b27c9723ea362292f0b97c431e6489fde4802dbe
vcommon <vcommon@126.com>
2015-09-09 14:28:17 +0800

    setup framework

---
+++ blogOc/blog/Controllers/BlogPostViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  blog
+//
+//  Created by peter on 15/9/9.
+//  Copyright (c) 2015年 lazyathome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BlogPostViewController : UIViewController
+
+
+@end
+
---
+++ blogOc/blog/Controllers/BlogPostViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  blog
+//
+//  Created by peter on 15/9/9.
+//  Copyright (c) 2015年 lazyathome. All rights reserved.
+//
+
+#import "BlogPostViewController.h"
+
+@interface BlogPostViewController ()
+
+@end
+
+@implementation BlogPostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
